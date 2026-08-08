29cca4ef9e5a1681e1f3c21cec99f275e5529b99
Akvis24 <akvis2022@gmail.com>
2026-08-08 06:22:34 +0200

    Add initial database and migrations

--- auth_app/models.py
+++ auth_app/models.py
@@ -33,5 +33,7 @@ class User(AbstractUser):
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=[]
 
+    objects=UserManager()
+
     def __str__(self):
         return self.email
--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES=[
+        ('to-do', 'To Do'),
+        ('in-progress', 'In Progress'),
+        ('review', 'Review'),
+        ('done', 'Done'),
+    ]
+
+    PRIORITY_CHOICES=[
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    board=models.ForeignKey('board_app.Board', on_delete=models.CASCADE, related_name='tasks')
+    title=models.CharField(max_length=150)
+    description=models.TextField(blank=True)
+    status=models.CharField(max_length=20, choices=STATUS_CHOICES, default='to-do')
+    priority=models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+    assignee=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='assigned_tasks', null=True, blank=True)
+    reviewer=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='review_tasks', null=True, blank=True)
+    due_date=models.DateField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    task=models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    content=models.TextField()
+    created_at=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'comment by {self.author} on {self.task}'
