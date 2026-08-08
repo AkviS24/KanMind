d4925f8c20868c6d77f6a8553380db03a3f5cbcd
Akvis24 <akvis2022@gmail.com>
2026-08-08 05:31:56 +0200

    Setup KanMind Data Structure and User Management System

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'auth_app',
+    'board_app',
+    'task_app',
 ]
+
+AUTH_USER_MODEL='auth_app.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
