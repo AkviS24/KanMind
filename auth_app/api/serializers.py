66501f1e8b3070c44ff23cd4206ad1095807c936
Akvis24 <akvis2022@gmail.com>
2026-08-08 08:16:20 +0200

    Add login Endpoint

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 
 from ..models import User
 
@@ -17,18 +18,35 @@ class RegistrationSerializer(serializers.ModelSerializer):
             'password':{'write_only': True},
         }
 
-        def validate(self, attrs):
-            if attrs['password'] != attrs['repeated_password']:
-                raise serializers.ValidationError({'password': 'Password´s don´t match.'})
-            return attrs
+    def validate(self, attrs):
+        if attrs['password'] != attrs['repeated_password']:
+            raise serializers.ValidationError({'password': 'Password´s don´t match.'})
+        return attrs
 
-        def create(self, validated_data):
-            validated_data.pop('repeated_password')
+    def create(self, validated_data):
+        validated_data.pop('repeated_password')
 
-            user=User.objects.create_user(
-                email=validated_data['email'],
-                password=validated_data['password'],
-                fullname=validated_data['fullname'],
-            )
+        user=User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            fullname=validated_data['fullname'],
+        )
 
-            return user
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email=serializers.EmailField()
+    password=serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user=authenticate(
+            email=attrs['email'],
+            password=attrs['password'],
+        )
+
+        if user is None:
+            raise serializers.ValidationError('Invalid email or password.')
+
+        attrs['user']=user
+        return attrs
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -3,16 +3,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, LoginSerializer
+
 
 class RegistrationView(APIView):
-    permission_classes=[]
+    permission_classes = []
 
     def post(self, request):
-        serializer=RegistrationSerializer(data=request.data)
+        serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
-            user=serializer.save()
-            token=Token.objects.create(user=user)
+            user = serializer.save()
+            token = Token.objects.create(user=user)
             return Response(
                 {
                     'token': token.key,
@@ -22,6 +23,32 @@ class RegistrationView(APIView):
                 },
                 status=status.HTTP_201_CREATED,
             )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class LoginView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, _ = Token.objects.get_or_create(user=user)
+
+            return Response(
+                {
+                    'token': token.key,
+                    'fullname': user.fullname,
+                    'email': user.email,
+                    'user_id': user.id,
+                },
+                status=status.HTTP_200_OK,
+            )
+
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST,
